71004bd1fb74bf9fcc689f7c4074c9b3612f69f4
cantwells <rjazantsev@gmail.com>
2021-04-17 22:05:39 +0300

    Get users from api

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
+import { useDispatch } from "react-redux";
 import "./App.css";
+import { fetchUsers } from "./redux/slices/usersSlice";
 
 function App() {
+  const dispatch = useDispatch();
+  dispatch(fetchUsers());
   return <div>Test</div>;
 }
 
