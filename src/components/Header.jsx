9a7ab5a7fcc9910b327b5a0fd94a3ce0787544ea
cantwells <rjazantsev@gmail.com>
2021-05-02 18:25:49 +0300

    Adding header for project

---
+++ src/components/Header.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+export const Header = ({ onOpenForm }) => {
+  return (
+    <nav className="px1 indigo">
+      <div className="nav-wrapper">
+        <div className="row">
+          <div className="col m6">
+            <a href="/" className="brand-logo">
+              <i className="material-icons small">person_outline</i>
+              Users
+            </a>
+          </div>
+          <div className="col m6">
+            <ul className="right">
+              <li>
+                <div className="menu-item pointer px1" onClick={onOpenForm}>
+                  Add user
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
