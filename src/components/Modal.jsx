83d1ce46897c951c8a3a40e57f68ee0234bcf90c
cantwells <rjazantsev@gmail.com>
2021-05-05 22:46:33 +0300

    Fixed bug with warning when close the modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -35,13 +35,14 @@ export const Modal = ({ onSubmit, onCloseForm, usrObj }) => {
           </div>
           <div className="modal-footer">
             <button
+              type="button"
               onClick={onCloseForm}
               className="waves-effect red waves-red btn left">Close
             </button>
             {
               usrObj
-              ? <button className="waves-effect waves-green btn right">Edit</button>
-              : <button className="waves-effect waves-green btn right">Add</button>
+              ? <button type="submit" className="waves-effect waves-green btn right">Edit</button>
+              : <button type="submit" className="waves-effect waves-green btn right">Add</button>
             }           
           </div>
         </form>
